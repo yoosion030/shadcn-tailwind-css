19f3ea0b8fb8d3103d6d97ff00a115d8039ed97e
yoosion030 <yoosioff@gmail.com>
2024-01-30 10:27:22 +0900

    feat: project init

---
+++ app/layout.tsx
@@ -0,0 +1,28 @@
+import { cn } from "@/lib/utils";
+import { Inter as FontSans } from "next/font/google";
+import "../app/globals.css";
+
+export const fontSans = FontSans({
+  subsets: ["latin"],
+  variable: "--font-sans",
+});
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <head />
+      <body
+        className={cn(
+          "min-h-screen bg-background font-sans antialiased",
+          fontSans.variable
+        )}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
